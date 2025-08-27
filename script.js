bfdd84a235c1cf04da44eedfb6532713286d62a6
RaihanNaim <imraihan.naim@gmail.com>
2025-08-27 14:49:33 +0600

    half of call button task is dome

--- script.js
+++ script.js
@@ -16,6 +16,25 @@ for (const icon of heartIcons) {
   icon.addEventListener("click", function () {
     displayHeartCount++;
     document.getElementById("heart-points").innerText = displayHeartCount;
-    // getInnerText('heart-points') = displayHeartCount;
   });
+}
+
+//❗❗❗❗❗❗❗Call button ❗❗❗❗❗❗❗
+const btnCalls = document.getElementsByClassName('btn-call');
+for(const btn of btnCalls){
+    btn.addEventListener('click', function(){
+        const text =  btn.parentNode.parentNode.children[0].innerText;
+        const callNumber = btn.parentNode.parentNode.children[2].innerText;
+
+        // display coins
+        const displayCoins = 0;
+        let coins = Number(document.getElementById('coins').innerText);
+        if(coins >= 20){
+            alert(`📞 Calling ${text} ${callNumber}...`);
+            document.getElementById('coins').innerText = coins - 20;
+        }else{
+            alert('❌ আপনার পর্যাপ্ত কয়েন নেই, কল করতে কমপক্ষে ২০ কয়েন লাগবে')
+        }
+        
+    })
 }
