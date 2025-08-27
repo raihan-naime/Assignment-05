1cbcc626f46475e009e49d3b96cb95a3c62efff5
RaihanNaim <imraihan.naim@gmail.com>
2025-08-27 16:15:34 +0600

    complete the copy points task

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 //❗❗❗❗❗❗❗ common functions ❗❗❗❗❗❗❗
 
-
-
-
-
-
-
-
-
-//❗❗❗❗❗❗❗display heart icon ❗❗❗❗❗❗❗
-
+//❗❗❗❗❗❗❗display heart Points ❗❗❗❗❗❗❗
 let displayHeartCount = 0;
 const heartIcons = document.getElementsByClassName("heart-icon");
 for (const icon of heartIcons) {
@@ -19,23 +10,35 @@ for (const icon of heartIcons) {
   });
 }
 
-//❗❗❗❗❗❗❗Call button ❗❗❗❗❗❗❗
-const btnCalls = document.getElementsByClassName('btn-call');
-for(const btn of btnCalls){
-    btn.addEventListener('click', function(){
-        const text =  btn.parentNode.parentNode.children[0].innerText;
+//❗❗❗❗❗❗❗display Copy Points ❗❗❗❗❗❗❗
+let displayCopyCount = 0;
+const CopyBtns = document.getElementsByClassName("btn-copy");
+for (const btn of CopyBtns) {
+    btn.addEventListener("click", function () {
         const callNumber = btn.parentNode.parentNode.children[2].innerText;
+        alert(`নাম্বার কপি হয়েছে ${callNumber}`);
+      displayCopyCount++;
+      document.getElementById("copy-point").innerText = displayCopyCount;
+    });
+}
 
-        // display coins
-        let coins = Number(document.getElementById('coins').innerText);
-        if(coins >= 20){
-            alert(`📞 Calling ${text} ${callNumber}...`);
-            document.getElementById('coins').innerText = coins - 20;
+//❗❗❗❗❗❗❗Call button ❗❗❗❗❗❗❗
+const btnCalls = document.getElementsByClassName("btn-call");
+for (const btn of btnCalls) {
+  btn.addEventListener("click", function () {
+    const text = btn.parentNode.parentNode.children[0].innerText;
+    const callNumber = btn.parentNode.parentNode.children[2].innerText;
 
-            // get history parent
-            const historyParent = document.getElementById('history');
-            const div = document.createElement('div');
-            div.innerHTML = `
+    // display coins
+    let coins = Number(document.getElementById("coins").innerText);
+    if (coins >= 20) {
+      alert(`📞 Calling ${text} ${callNumber}...`);
+      document.getElementById("coins").innerText = coins - 20;
+
+      // get history parent
+      const historyParent = document.getElementById("history");
+      const div = document.createElement("div");
+      div.innerHTML = `
             <div
                 class="flex justify-between items-center text-xl mb-2 bg-[#fafafa] p-5 rounded-lg"
               >
@@ -45,12 +48,15 @@ for(const btn of btnCalls){
                 </div>
                 <p class="text-[#2e2e2e]">${new Date().toLocaleTimeString()}</p>
               </div>
-            `
-            historyParent.appendChild(div);
-
-        }else{
-            alert('❌ আপনার পর্যাপ্ত কয়েন নেই, কল করতে কমপক্ষে ২০ কয়েন লাগবে')
-        }
-        
-    })
+            `;
+      historyParent.appendChild(div);
+    } else {
+      alert("❌ আপনার পর্যাপ্ত কয়েন নেই, কল করতে কমপক্ষে ২০ কয়েন লাগবে");
+    }
+  });
 }
+
+//❗❗❗❗❗❗❗Clear button ❗❗❗❗❗❗❗
+document.getElementById("btn-clear").addEventListener("click", function () {
+  document.getElementById("history").innerHTML = "";
+});
